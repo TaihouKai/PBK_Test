3888b4c9c6eb101da8c31a927d48ee1df3145283
Xiangyu Su <suxiangyu2016@gmail.com>
2021-05-29 11:48:38 +0900

    add aggre

--- app/src/main/java/com/example/pbk_test/BLS01.java
+++ app/src/main/java/com/example/pbk_test/BLS01.java
@@ -9,6 +9,7 @@ import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.CryptoException;
 import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
@@ -17,6 +18,9 @@ import it.unisa.dia.gas.crypto.jpbc.signature.bls01.generators.BLS01KeyPairGener
 import it.unisa.dia.gas.crypto.jpbc.signature.bls01.generators.BLS01ParametersGenerator;
 import it.unisa.dia.gas.crypto.jpbc.signature.bls01.params.BLS01KeyGenerationParameters;
 import it.unisa.dia.gas.crypto.jpbc.signature.bls01.params.BLS01Parameters;
+import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.jpbc.Pairing;
+import it.unisa.dia.gas.jpbc.Field;
 import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
 
 
@@ -61,6 +65,17 @@ public class BLS01 {
         return signature;
     }
 
+    public byte[] aggregate(List<byte[]> signatures) {
+        Pairing pairing = PairingFactory.getPairing(MainActivity.getCacheFile("a.properties", context).toPath().toString()));
+        Field f = pairing.getG1();
+        Element aggregated = f.newElement(1);
+        for (byte[] signature: signatures) {
+            Element temp = pairing.getG1().newElementFromBytes(signature);
+            aggregated = aggregated.mul(temp);
+        }
+        return aggregated.toBytes();
+    }
+
     public boolean verify(byte[] signature, String message, CipherParameters publicKey) {
         byte[] bytes = message.getBytes();
 
@@ -70,6 +85,22 @@ public class BLS01 {
 
         return signer.verifySignature(signature);
     }
+
+//    public boolean verifyAggre(byte[] aggregated, List<String> messages, @NotNull List<CipherParameters> publicKeys) {
+//        Element aggre = pairing.getG1().newElementFromBytes(aggregated);
+//
+//        for (String message: messages) {
+//            Element h = pairing.getG1().newElementFromHash(hash, 0, hash.length);
+//        }
+//        Element h = pairing.getG1().newElementFromHash(hash, 0, hash.length);
+//
+//        Element temp1 = pairing.pairing(sig, publicKey.getParameters().getG());
+//        Element temp2 = pairing.pairing(h, publicKey.getPk());
+//
+//        return temp1.isEqual(temp2);
+//    }
+
+
 
 
 }
