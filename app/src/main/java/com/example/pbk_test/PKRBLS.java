e7728cd399f83d729f223bd968fe68aa38693169
Xiangyu Su <suxiangyu2016@gmail.com>
2021-06-05 11:22:34 +0900

    verifyshow

--- app/src/main/java/com/example/pbk_test/PKRBLS.java
+++ app/src/main/java/com/example/pbk_test/PKRBLS.java
@@ -22,7 +22,6 @@ import it.unisa.dia.gas.jpbc.Element;
 import it.unisa.dia.gas.jpbc.Pairing;
 import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
 
-
 public class PKRBLS {
 
     private final Context context;
